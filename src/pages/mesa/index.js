4ab9bf5aa2ddf05c686d790b48f6b1de917f082b
Augusto Albuquerque <dhelp@hotmail.com>
2020-05-13 11:43:49 -0300

    nova tela de mesa

--- src/pages/mesa/index.js
+++ src/pages/mesa/index.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
+
+
 //import DataGrid from 'react-data-grid';
+
+import Button01 from '@material-ui/core/Button';
+
+import { EditOutlined, DeleteOutline } from '@material-ui/icons';
 
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
@@ -7,6 +13,7 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
+
 
 
 
@@ -23,22 +30,27 @@ export default function Mesa() {
     const [ramal, setRamal] = useState('');
     const [listaMesa, setaListaMesa] = useState([]);
 
-    
-
-    //const rows = listaMesa;
 
 
-    function recall(){
 
-        api.get('mesa').then(
+
+  async  function recall() {
+
+        
+       await api.get('mesa').then(
             response => {
                 setaListaMesa(response.data)
-                
+
             }
         )
     }
 
-  
+
+    function editar(){
+
+
+    }
+
 
     async function handMesa(e) {
         e.preventDefault();
@@ -57,7 +69,12 @@ export default function Mesa() {
         }
     }
 
-
+    async function handleClick(e) {
+        //e.preventDefault();
+        await api.delete(`mesa/${e}`);
+        recall();
+        
+    }
 
 
     useEffect(() => {
@@ -69,118 +86,111 @@ export default function Mesa() {
         )
     }, [])
 
-
+   
 
     return (
         <Container fluid>
-        <div className="mesa-content">
+
             <h1>Cadastro de Mesas e Ramal</h1>
-            <hr />
-            
-            <section className="cadastro">
-                <form onSubmit={handMesa}>
 
-                
-                    <div className="row">
-                        <label>
-                            Mesa:
-                            </label>
-                    <input
+           
+            <hr />
+
+
+
+
+
+            <Form className="cadastro" onSubmit={handMesa}>
+                <Row>
+                    <Col>
+                    <Form.Group controlId="formGroupEmail">
+                            
+                            <Form.Control
                                 type="text"
-                                placeholder="Informe o número da mesa"
+                                placeholder="id"
+                                
+                                hidden
+                                />
+                        </Form.Group>
+                        <Form.Group controlId="formGroupEmail">
+                            <Form.Label>MESA</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Informe o nome/número da mesa"
                                 value={mesa}
-                                onChange={e => setMesa(e.target.value)}
-                            />
-                        
-                        </div>
-                        <div className="row">
-                        <label>
-                            Ramal:
-                            </label>
-                    <input
-                    type="text"
-                                placeholder="Informe o ramal"
+                                onChange={e => setMesa(e.target.value)} />
+                        </Form.Group>
+
+                        <Form.Group controlId="formGroupPassword">
+                            <Form.Label>RAMAL</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Informe o ramal da mesa"
                                 value={ramal}
-                                onChange={e => setRamal(e.target.value)}
-                            />
-                        
-                        </div>
-                        <div className="row">
-                        <button className="button" type="submit">Cadastrar</button>
-                    </div>
+                                onChange={e => setRamal(e.target.value)} />
+                        </Form.Group>
 
+                        <Form.Group controlId="formGroupButton">
+                            <Form.Label></Form.Label>
 
-                    
+                            <Button name="salvar" className="buttonsalvar" type="submit" variant="success">Cadastrar</Button>
+                        </Form.Group>
 
+                    </Col>
+                </Row>
+                
 
-                </form>
-               </section>
-
-
-
-
-<Form onSubmit={handMesa}>
-  <Row>
-    <Col>
-    <Form.Group controlId="formGroupEmail">
-    <Form.Label>MESA</Form.Label>
-    <Form.Control 
-                type="text" 
-                placeholder="Informe o nome/número da mesa"
-                value={mesa}
-                onChange={e => setMesa(e.target.value)} />
-  </Form.Group>
-    </Col>
-    <Col>
-    <Form.Group controlId="formGroupPassword">
-    <Form.Label>RAMAL</Form.Label>
-    <Form.Control 
-                type="text" 
-                placeholder="Informe o ramal da mesa"
-                value={ramal}
-                onChange={e => setRamal(e.target.value)} />
-  </Form.Group>
-    </Col>
-    <Col>
-    <Form.Group controlId="formGroupButton">
-    <Form.Label></Form.Label>
-      
-      <Button className="button" type="submit" variant="success">Salvar</Button>
-      </Form.Group>
-    </Col>
-  </Row>
-</Form>
+            </Form>
             <hr />
 
-            
+
 
             <div className="listagem">
-            <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th hidden >id</th>
-                <th>MESA</th>
-                <th>RAMAL</th>
-                </tr>
-            </thead>
-            <tbody>
-                {listaMesa.map(l => (
-                <tr key={l.id}>
-                <td hidden>{l.id}</td>
-                <td>{l.mesa}</td>
-                <td>{l.ramal}</td>
-                </tr>
-                ))}
-                
-                
-            </tbody>
-            </Table>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr >
+                            <th hidden >id</th>
+                            <th >AÇÃO </th>
+                            <th >MESA</th>
+                            <th>RAMAL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listaMesa.map(l => (
+                            <tr key={l.id}>
+                                <td hidden>{l.id}</td>
+                                <th>
+                                
+                                
+
+                                <Button01  className="buttonAcao"
+                                        startIcon={<EditOutlined/>}
+                                        item={l.id}
+                                        onClick={(e) => handleClick(l.id)}
+                                    ></Button01>
+
+                                    <Button01 className="buttonAcao"
+                                        startIcon={<DeleteOutline/>}
+                                        item={l.id}
+                                        onClick={(e) => handleClick(l.id)}
+                                    ></Button01>
+
+
+                                </th>
+                                <td>{l.mesa}</td>
+                                <td>{l.ramal}</td>
+                            </tr>
+                        ))}
+
+
+                    </tbody>
+                </Table>
             </div>
 
 
-            
 
-        </div>
+
+
         </Container>
 
 
