544ac5c57491b57f3f9d0431f44b3f4e52fa2a68
Augusto Albuquerque <dhelp@hotmail.com>
2020-06-25 15:35:16 -0300

    soma de atendimento e telefone

--- src/pages/fila/index.js
+++ src/pages/fila/index.js
@@ -268,7 +268,7 @@ export default function Index() {
 
       }
     )
-  }, [])
+  }, [n])
 
 
   useEffect(() => {
@@ -278,7 +278,7 @@ export default function Index() {
 
       }
     )
-  }, [])
+  }, [n])
 
 
 
